4747665be878e29e37778e3154cb01af8aa2da15
dibsnvas <d_beisenbayeva@kbtu.kz>
2024-06-12 18:19:21 +0500

    dbFixed

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,12 @@ import express from 'express';
 import mongoose from 'mongoose';
 import eventRouter from './events/event-router';  // Adjust the path as necessary
 import authRouter from './auth/auth-router';  // Adjust the path as necessary
+import connectDB from './db';
 
 const app = express();
 const port = 3000;
+
+connectDB()
 
 // Middleware
 app.use(express.json());
@@ -13,14 +16,6 @@ app.use(express.json());
 app.use('/api', eventRouter);
 app.use('/api', authRouter);
 
-// Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/events')
-  .then(() => {
-    console.log('Connected to MongoDB');
-    app.listen(port, () => {
-      console.log(`Server is running on http://localhost:${port}`);
-    });
-  })
-  .catch((error) => {
-    console.error('Error connecting to MongoDB', error);
-  });
+app.listen(8000, () => {
+  console.log('Server started')
+})
